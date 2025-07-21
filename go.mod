bab7607cfcfda5ff1ffb93237e4fa823491586f2
Phillip-England <phillip.mark.england@gmail.com>
2025-07-21 05:10:17 -0500

    moving over

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/Phillip-England/marki
 go 1.24.4
 
 require (
+	github.com/alecthomas/chroma/v2 v2.2.0 // indirect
+	github.com/dlclark/regexp2 v1.7.0 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/yuin/goldmark v1.7.12 // indirect
+	github.com/yuin/goldmark-highlighting/v2 v2.0.0-20230729083705-37449abec8cc // indirect
+	github.com/yuin/goldmark-meta v1.1.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,18 +4,26 @@ import (
 	"fmt"
     "path/filepath"
     "io/fs"
+	"os"
+	"bytes"
 	"github.com/fsnotify/fsnotify"
+	"github.com/yuin/goldmark"
+    "github.com/yuin/goldmark/parser"
+    "github.com/yuin/goldmark/renderer/html"
+	"github.com/yuin/goldmark-meta"
+	chromahtml "github.com/alecthomas/chroma/v2/formatters/html"
+	highlighting "github.com/yuin/goldmark-highlighting/v2"
 )
 
 func main() {
-	err := startRuntime("./testmd", "./out")
+	err := startRuntime("./testmd", "./out", "dracula")
 	if err != nil {
         fmt.Printf("MARKI ERROR: %s\n", err.Error())
         main()
 	}
 }
 
-func startRuntime(inDir string, outDir string) error {
+func startRuntime(inDir string, outDir string, theme string) error {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		return err
@@ -35,7 +43,7 @@ func startRuntime(inDir string, outDir string) error {
 					errChan <- fmt.Errorf("fsnotify: event channels closed")
 					return
 				}
-				err := onChange(inDir, outDir, event)
+				err := onChange(inDir, outDir, theme, event)
 				if err != nil {
 					errChan <- err
 					return
@@ -56,7 +64,7 @@ func startRuntime(inDir string, outDir string) error {
 	return nil
 }
 
-func onChange(inDir string, outDir string, event fsnotify.Event) error {
+func onChange(inDir string, outDir string, theme string, event fsnotify.Event) error {
     err := filepath.WalkDir(inDir, func(path string, d fs.DirEntry, err error) error {
         if err != nil {
             return err
@@ -68,10 +76,11 @@ func onChange(inDir string, outDir string, event fsnotify.Event) error {
         if ext != ".md" {
             return nil
         }
-        mdFile, err := NewMarkdownFile(path)
+		mdFile, err := NewMarkdownFile(path, theme)
         if err != nil {
             return err
         }
+		fmt.Println(mdFile.Path)
         return nil
     })
     if err != nil {
@@ -82,12 +91,50 @@ func onChange(inDir string, outDir string, event fsnotify.Event) error {
 
 
 type MarkdownFile struct {
-    path string
+    Path string
+	Text string
+	Theme string
+	Html string
+	Meta map[string]any
 }
 
-func NewMarkdownFile(path string) (MarkdownFile, error) {
-    mdFile := MarkdownFile {
-        path: path,
-    }
+func NewMarkdownFile(path string, theme string) (MarkdownFile, error) {
+	var mdFile MarkdownFile
+	mdBytes, err := os.ReadFile(path)
+	if err != nil {
+		return mdFile, err
+	}
+    mdFile.Text = string(mdBytes)
+	mdFile.Path = path
+	mdFile.Theme = theme
+	md := goldmark.New(
+			goldmark.WithExtensions(
+				meta.Meta,
+				highlighting.NewHighlighting(
+					highlighting.WithStyle(theme),
+					highlighting.WithFormatOptions(
+						chromahtml.WithLineNumbers(true),
+					),
+				),
+			),
+			goldmark.WithParserOptions(
+				parser.WithAutoHeadingID(),
+			),
+			goldmark.WithParserOptions(
+				parser.WithAttribute(),
+			),
+			goldmark.WithRendererOptions(
+				html.WithHardWraps(),
+				html.WithXHTML(),
+				html.WithUnsafe(),
+			),
+		)
+	var buf bytes.Buffer
+	context := parser.NewContext()
+	if err := md.Convert(mdBytes, &buf, parser.WithContext(context)); err != nil {
+		return mdFile, err
+	}
+	mdFile.Html = buf.String()
+	mdFile.Meta = meta.Get(context)
     return mdFile, nil
 }
