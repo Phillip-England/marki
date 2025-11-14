221764b04b46e514d73c2222003b66c27bc03c4a
Phillip England <phillip.mark.england@gmail.com>
2025-11-14 06:56:09 -0600

    got marki accepting md content from command line

--- main.go
+++ main.go
@@ -3,17 +3,19 @@ package main
 import (
 	"fmt"
 
+	"github.com/phillip-england/marki/cmd"
 	"github.com/phillip-england/whip"
 )
 
 func main() {
 
-	cli, err := whip.New(NewDefaultCmd)
+	cli, err := whip.New(cmd.NewDefault)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
-	cli.At("convert", NewConvertCmd)
+	cli.At("convert", cmd.NewConvert)
+	cli.At("run", cmd.NewRun)
 
 	err = cli.Run()
 	if err != nil {
